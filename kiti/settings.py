ac656f5330bf4ead1199cf0162c1032334a4259b
Simanta Roy <roy35-909@diu.edu.bd>
2024-01-26 00:00:16 +0600

    allow cors origin

--- kiti/settings.py
+++ kiti/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'corsheaders',
     ## Authentication
     'authentication',
     'Foods',
@@ -52,6 +54,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'https://kiti-food-delivery-robo.vercel.app',
+
 ]
 
 ROOT_URLCONF = 'kiti.urls'
